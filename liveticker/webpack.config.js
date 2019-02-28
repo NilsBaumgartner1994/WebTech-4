12bc55806015cb53b5bd5892d4f4147d49e8bca1
joarndt <joarndt94@gmail.com>
2019-02-28 15:40:14 +0100

    Revert "added jquery"
    This reverts commit 28fdbbac9035f701a2ad813d1b766b25877a53a8.

--- liveticker/webpack.config.js
+++ liveticker/webpack.config.js
@@ -1,21 +1,7 @@
 const path = require('path');  // node.js uses CommonJS modules
-var webpack = require('webpack');
-
 
 module.exports = {
   entry: './clientsrc/client.js',  // the entry point
-  plugins: [
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jquery: "jQuery",
-            "window.jQuery": "jquery"
-        })
-    ],
-  resolve: {
-        alias: {
-            jquery: "jquery/src/jquery"
-        }
-    },
   output: {
     filename: 'bundle.js',  // the output filename
     path: path.resolve(__dirname, 'public')  // fully qualified path
